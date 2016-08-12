f5721cad57370a8425f7e9917fd558568d36bf19
Mateus <mateusflavio@gmail.com>
2016-08-11 23:04:31 -0300

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Mateus',
+            cpf=32783355892,
+            email='mateusflavio@gmail.com',
+            phone='16-992966600'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_create_at(self):
+        self.assertIsInstance(self.obj.create_at, datetime)
